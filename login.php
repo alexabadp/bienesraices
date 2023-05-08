c77372c8242ed674c00556561c52356bc89fc864
alexabadp <aalex.abad96@gmail.com>
2023-05-08 12:06:24 -0500

    inicio de sesion

--- login.php
+++ login.php
@@ -1,4 +1,68 @@
 <?php
+
+require 'includes/config/database.php';
+$db = conectarDB();
+// Autenticar el usuario
+
+$errores = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // echo "<pre>";
+    // var_dump($_POST);
+    // echo "</pre>";
+
+    $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+
+    $password =  mysqli_real_escape_string($db, $_POST['password']);
+
+    if (!$email) {
+        $errores[] = 'El email es obligatorio o no es valido';
+    }
+
+    if (!$password) {
+        $errores[] = 'El password es obligatorio';
+    }
+
+    if (empty($errores)) {
+
+        // Revisar si el usuario existe
+        $query = "SELECT *  FROM usuarios WHERE email = '$email';";
+        $resultado = mysqli_query($db, $query);
+
+        // var_dump($resultado);
+
+        if ($resultado->num_rows) {
+            // Revisar si el password es correcto
+            $usuario = mysqli_fetch_assoc($resultado);
+
+            // Verificar si el password es correcto o no
+            $auth = password_verify($password, $usuario['password']);
+
+            if ($auth) {
+                // El usuario esta autenticado
+                session_start();
+
+                //Llenar el arreglo de la sesion
+                $_SESSION['usuario'] = $usuario['email'];
+                $_SESSION['login'] = true;
+
+                echo "<pre>";
+                var_dump($_SESSION);
+                echo "</pre>";
+            } else {
+                $errores[] = 'El password es incorrecto';
+            }
+        } else {
+            $errores[] = "El usuario no existe";
+        }
+    }
+
+    // echo "<pre>";
+    // var_dump($errores);
+    // echo "</pre>";
+}
+
+// Incluye el header
 require 'includes/funciones.php';
 incluirTemplate('header');
 ?>
@@ -6,15 +70,21 @@ incluirTemplate('header');
 <main class="contenedor seccion contenido-centrado">
     <h1>Iniciar Sesion</h1>
 
-    <form class="formulario">
+    <?php foreach ($errores as $error) : ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach ?>
+
+    <form method="POST" class="formulario">
         <fieldset>
             <legend>Email y Password</legend>
 
             <label for="email">E-mail</label>
-            <input type="email" placeholder="Tu Email" id="email" />
+            <input type="email" name="email" placeholder="Tu Email" id="email" required />
 
             <label for="password">Password</label>
-            <input type="password" placeholder="Tu Password" id="password" />
+            <input type="password" name="password" placeholder="Tu Password" id="password" required />
 
         </fieldset>
 
